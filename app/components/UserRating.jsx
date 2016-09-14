a6a05961bc9d4c1416dbd5b0eadd7e741cc8a25d
Gamerlazer <gamerlazer@gmail.com>
2016-09-14 12:13:52 -0700

    Pulling for merge

--- app/components/UserRating.jsx
+++ app/components/UserRating.jsx
@@ -1,40 +1,42 @@
 import React, {Component} from 'react';
-import StarRatingComponent from 'react-star-rating-component';
 import ReactStars from 'react-stars'
-
 
 export default class UserRating extends Component {
   constructor(props) {
     super(props);
     this.state = {
       // Setting testing as 4
-      rating: 4
+      rating: 4,
+      editable: true
     }
   }
+
+  updateRating (newRating) {
+    console.log('this happens ')
+    this.setState({
+      rating: newRating
+    });
+  }
+
+  ratingChanged (newRating) {
+    console.log(newRating)
+  }
+
+  handleRatingClick (e, data) {
+    console.log(e, 'This is e');
+    console.log(data, 'This is data')
+    alert('You left a ' + data.rating + ' star rating for ' + data.caption);
+  }
+
   render () {
     return (
       <div>
         <div className="star-rating"></div>
-        <div>{this.state.rating}</div>
-        <StarRatingComponent 
-            name="rate1" 
-            starCount={5}
-            // editing={false}
-            // renderStarIcon={() => <span></span>}
-            value={5}
-        />
-        <ReactStars
-          count={5}
-          // onChange={ratingChanged},
-          size={24}
-          color2={'#ffd700'} />
+        <div>My current rating: {this.state.rating} </div>
+        <ReactStars count={5} onChange={this.ratingChanged} size={24} color2={'#ffd700'} />
       </div>
     )
   }
 }
-// <StarRatingComponent 
-//     name="rate1" 
-//     starCount={10}
-//     value={rating}
-//     onStarClick={this.onStarClick.bind(this)}
-// />
+
+
