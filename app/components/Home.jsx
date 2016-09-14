0cffae83857d799b433dafc2244a9234b711fbdd
Gamerlazer <gamerlazer@gmail.com>
2016-09-13 19:24:47 -0700

    Update React Routes

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import AuctionEntry from './auctionentry.jsx';
+import AuctionEntry from './Listing.jsx';
 
 export default class Auction extends Component {
   constructor(props) {
--- app/index.jsx
+++ app/index.jsx
@@ -2,27 +2,31 @@ import React, {Component} from 'react';       //  Import react and react-router 
 import ReactDOM from 'react-dom';
 import {Router, Route, Link, hashHistory, IndexRoute, browserHistory} from 'react-router';
 import App from './components/App.jsx';
-import Layout from './components/Layout.jsx';
+import Landing from './components/Landing.jsx';
 import Home from './components/Home.jsx';
 import NotFound from './components/notfound.jsx';
 import MyBids from './components/MyBids.jsx';
 import PostItem from './components/PostItem.jsx';
 import ProductDetail from './components/ProductDetail.jsx';
 import Account from './components/Account.jsx';
-import History from './components/history.jsx';
+
 
 
 ReactDOM.render((                             //  Set up routes to navigate between different pages
   <Router history={browserHistory}>
     <Route path='/' component={App} >
-      <IndexRoute component={Layout} />
+      <IndexRoute component={Landing} />
       <Route path='/home' component={Home} />
       <Route path='/item/:id' component={ProductDetail}/>
       <Route path='/dashboard' component= {MyBids} />
-      <Route path='/postitem' component = {PostItem}/>
+      <Route path='/postitem' component = {PostItem} />
       <Route path='/account' component={Account} />
-      <Route path='/history' component={History} />
       <Route path='*' component={NotFound} />
     </Route>
   </Router>
 ), document.getElementById('root'));
+
+
+/********** REMOVING CODE ****************/
+// import History from './components/history.jsx';
+// <Route path='/history' component={History} />
