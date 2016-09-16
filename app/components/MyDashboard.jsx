2634a40ee5c8025f434fb6ba7a64a232b8738bf0
Sean <shonum@gmail.com>
2016-09-16 12:18:42 -0700

    Page change on render

--- app/components/MyDashboard.jsx
+++ app/components/MyDashboard.jsx
@@ -8,10 +8,12 @@ export default class MyDashboard extends Component {
   constructor(props) {
     super(props);
     // view will be 'sales' or 'bids'
+    console.log(this.props)
     this.state = {
-      view: this.props.params.view,
+      view: this.props.route.type,
       activeItems: []
     };
+    console.log('THIS.STATE.VIEW!', this.state.view)
   }
 
   getSalesItems () {
@@ -48,22 +50,22 @@ export default class MyDashboard extends Component {
   }
 
   render() {
-    console.log(this.state.view)
-    if (this.state.view === 'unauthorized') {
+    console.log(this.props.params.view)
+    if (this.props.params.view === 'unauthorized') {
       return (
         <div>
           Sorry dude, page was not found!
         </div>
       );
     }
-    if (this.state.view === 'sales') {
+    if (this.props.params.view === 'sales') {
       return (
         <div>
           This is the sales page
         </div>
       )
     }
-    if (this.state.view === 'bids') {
+    if (this.props.params.view === 'bids') {
       return (
         <div>
           This is the bids page
--- app/helpers.js
+++ app/helpers.js
@@ -7,9 +7,16 @@ export const calcPrice = function(startPrice, endPrice, startDate, endDate) {
 
 export const calcTime = function(endDate) {
 
-  var countdown = require('countdown');
+  var duration = Math.max(Date.parse(endDate) - Date.now(), 0);
+  var seconds = parseInt((duration / 1000) % 60);
+  var minutes = parseInt((duration / (1000 * 60)) % 60);
+  var hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+  var days = parseInt(((duration) / (1000 * 60 * 60 * 24)) % 365);
 
-  var timeleft = countdown(null, new Date(endDate), countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 2).toString();
-  return timeleft;
+  days = (days < 10) ? '0' + days : days;
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
 
+  return days + ' days  ' + hours + ':' + minutes + ':' + seconds + ' hours';
 };
